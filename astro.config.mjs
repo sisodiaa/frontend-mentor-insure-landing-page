df150617acb07fc21e33292b4cd919b3e780ffc1
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2023-06-22 14:22:37 +0530

    Modify astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  site: "https://sisodiaa.github.io/",
+  repo: "/frontend-mentor-insure-landing-page",
   experimental: {
     assets: true,
   },
